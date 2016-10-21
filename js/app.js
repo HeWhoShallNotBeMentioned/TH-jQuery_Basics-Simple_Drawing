2baf08e394d2027a17d1d582f2ef243667b2406c
Chris Underwood <cunderwoodmn@gmail.com>
2016-10-20 21:42:25 -0700

    allowed the cursor to write to the canvas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 //Problem: No user interaction causes no change to application
 //Solution: When user interacts cause changes appropriately
 var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
 
 //When clicking on control list items
 $(".controls").on("click", "li", function(){
@@ -44,4 +48,21 @@ $("#addNewColor").click(function(){
 
 
 //On mouse events on the canvas
+$canvas.mousedown(function(e){
+  lastEvent = e;
+  mouseDown = true;
+}).mousemove(function(e){
   //Draw lines
+  if(mouseDown) {
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = color;
+    context.stroke();
+    lastEvent = e;
+  }
+}).mouseup(function(){
+  mouseDown = false;
+}).mouseleave(function(){
+  $canvas.mouseup();
+});
