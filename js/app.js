21070664bed5db1e38f278c690313264ba9ef381
Chris Underwood <cunderwoodmn@gmail.com>
2016-10-20 17:48:11 -0700

    activate color buttons and store the color

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 //Problem: No user interaction causes no change to application
 //Solution: When user interacts cause changes appropriately
+var color = $(".selected").css("background-color");
 
 //When clicking on control list items
+$(".controls").on("click", "li", function(){
   //Deselect sibling elements
+  $(this).siblings().removeClass("selected");
   //Select clicked element
+  $(this).addClass("selected");
+  //cache current color
+  color = $(this).css("background-color");
+});
 
 //When "New Color" is pressed
+
   //Show color select or hide the color select
 
 //When color sliders change
